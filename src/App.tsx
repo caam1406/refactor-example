a159479127da67a6483bb9b4ea7bdf509b07ccd3
Carlos Matos <caam1406@gmail.com>
2022-05-23 14:56:18 -0300

    refactor: uncoupled interfaces from app

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,15 @@ import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
-
 // import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
-
 import { api } from './services/api';
-
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { GenreResponseProps } from './interfaces/genreResponseProps.I';
+import { MovieProps } from './interfaces/movieProps.I';
 
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
