eb876295a891503dc471b2dc27b5ae03246f0cdf
Carlos Matos <caam1406@gmail.com>
2022-05-24 20:12:02 -0300

    refactor: finished component content logic

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,13 @@
-export function Content() {
-  // Complete aqui
+import { IMovieProps } from "../interfaces/movieProps.I";
+import { MovieCard } from "./MovieCard";
+
+export function Content({movies}: {movies: IMovieProps[]}) {
+  return(
+  <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>);
 }
