0bc35e9cddaf6236e1a782bd1c5058bb37e6bd02
Carlos Matos <caam1406@gmail.com>
2022-05-24 21:10:43 -0300

    refactor: uncoupled sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,20 @@
-export function SideBar() {
-  // Complete aqui
+import { IGenreResponseProps } from "../interfaces/genreResponseProps.I";
+import { Button } from "./Button";
+
+
+export function SideBar(props: { genres: IGenreResponseProps[]; handleClickButton: (arg0: number) => void; selectedGenreId: number; }) {
+  return(<nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>);
 }
